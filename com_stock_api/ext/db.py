2a8d4fec617fab5b76dbd75fe6af181f44576fb0
Minsol Jeong <minsoljeong@Minsols-MacBook-Air.local>
2020-11-03 16:28:54 +0900

    1

--- com_stock_api/ext/db.py
+++ com_stock_api/ext/db.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects import registry
-import mysql.connector
 import sqlalchemy.dialects.sqlite
 import pymysql
 
