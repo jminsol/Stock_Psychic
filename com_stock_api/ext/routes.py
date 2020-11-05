32bde44a1242fed54124960b06a26d584093bed1
Minsol Jeong <minsoljeong@Minsols-MacBook-Air.local>
2020-11-05 15:39:09 +0900

    update

--- com_stock_api/ext/routes.py
+++ com_stock_api/ext/routes.py
@@ -4,7 +4,7 @@ from com_stock_api.resources.yhfinance import YHFinance, YHFinances, TeslaGraph,
 from com_stock_api.resources.recent_news import RecentNews, AppleNews, TeslaNews
 from com_stock_api.resources.investingnews import Investing, AppleSentiment, TeslaSentiment
 from com_stock_api.resources.nasdaq_prediction import NasdaqPrediction, NasdaqPredictions, ApplePredGraph, TeslaPredGraph
-from com_stock_api.resources.uscovid import USCovid, USCovids
+from com_stock_api.resources.uscovid import USCovid, USCovids, USNewCases, CANewCases
 
 from com_stock_api.resources.member import Member, Members, Auth, Access, HighChurnMembers
 from com_stock_api.resources.board import Board, Boards
@@ -15,7 +15,7 @@ from com_stock_api.resources.recommend_stock import RecommendStock, RecommendSto
 from com_stock_api.resources.korea_covid import KoreaCovid,KoreaCovids
 from com_stock_api.resources.kospi_pred import Kospi,Kospis,lgchem_pred,lginnotek_pred
 from com_stock_api.resources.korea_finance import Stock,Stocks,lgchem,lginnotek
-from com_stock_api.resources.korea_news import News,News_
+from com_stock_api.resources.korea_news import News,News_,Lgchem_Label,Lginnotek_Label
 from com_stock_api.resources.korea_news_recent import RNews,RNews_, lgchemNews,lginnoteknews
 
 def initialize_routes(api):
@@ -31,12 +31,13 @@ def initialize_routes(api):
     api.add_resource(AppleSentiment, '/nasdaq/apple_sentiment')
     api.add_resource(TeslaSentiment, '/nasdaq/tesla_sentiment')
     api.add_resource(USCovid, '/nasdaq/uscovid')
+    api.add_resource(USNewCases, '/nasdaq/us_new_cases')
+    api.add_resource(CANewCases, '/nasdaq/ca_new_cases')
 
     api.add_resource(Members, '/api/members')
     api.add_resource(Member, '/api/member/<string:email>')
     api.add_resource(Auth, '/api/auth')
     api.add_resource(HighChurnMembers, '/api/highchurnmembers')
-    print('=============== route.py')
     api.add_resource(Access, '/api/access')
     api.add_resource(Boards, '/api/boards')
     api.add_resource(Board, '/api/board/<string:id>')
@@ -46,15 +47,15 @@ def initialize_routes(api):
     api.add_resource(Tradings, '/api/tradings/<string:email>')
     api.add_resource(Trading, '/api/trading/<string:id>')
     api.add_resource(RecommendStocks, '/api/recommend-stocks')
-    api.add_resource(RecommendStock, '/api/recommend-stock')
+    api.add_resource(RecommendStock, '/api/recommend-stock/<string:id>')
 
-    api.add_resource(KoreaCovid,'/kospi/koreacovid/<string:id>')
-    api.add_resource(KoreaCovids,'/kospi/koreacovids')
-    api.add_resource(News, '/kospi/news')
-    api.add_resource(News_, '/kospi/news_') 
+    #api.add_resource(Home,'/kospi')
+    api.add_resource(KoreaCovid,'/kospi/koreacovid')
+    api.add_resource(lgchemNews,'/kospi/lgchemNews')
+    api.add_resource(lginnoteknews,'/kospi/lginnoteknews')
+    api.add_resource(Lgchem_Label, '/kospi/lgchem_label')
+    api.add_resource(Lginnotek_Label, '/kospi/lginnotek_label')
     api.add_resource(lgchem,'/kospi/lgchem')
     api.add_resource(lginnotek,'/kospi/lginnotek')
-    api.add_resource(lgchemNews,'/kospi/lgchemnews')
-    api.add_resource(lginnoteknews,'/kospi/lginnoteknews')
     api.add_resource(lgchem_pred, '/kospi/lgchem_pred')
     api.add_resource(lginnotek_pred, '/kospi/lginnotek_pred')
